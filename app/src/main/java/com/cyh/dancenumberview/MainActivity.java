61a957f3581b939b8bac7f4c85434da9747fbfc6
chenyuhang <chenyuhang@xinc818.group>
2021-11-10 14:33:11 +0800

    Demo展示

--- app/src/main/java/com/cyh/dancenumberview/MainActivity.java
+++ app/src/main/java/com/cyh/dancenumberview/MainActivity.java
@@ -3,12 +3,33 @@ package com.cyh.dancenumberview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private DanceNumberView danceNumberView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+    private void initView()
+    {
+        danceNumberView = findViewById(R.id.activity_main_danceview);
+        danceNumberView.setText("123,456");
+        danceNumberView.start();
+        Handler h = new Handler();
+        Random random = new Random(1000);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                danceNumberView.setText("4,323,222");
+                danceNumberView.start();
+            }
+        },2000);
     }
 }
