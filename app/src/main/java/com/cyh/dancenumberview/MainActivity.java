8369b2c7d21ca1d4ef7e54c267cf9b904289d0d0
chenyuhang <chenyuhang@xinc818.group>
2021-11-10 15:55:32 +0800

    Demo展示

--- app/src/main/java/com/cyh/dancenumberview/MainActivity.java
+++ app/src/main/java/com/cyh/dancenumberview/MainActivity.java
@@ -10,26 +10,34 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private DanceNumberView danceNumberView;
+    private final int delayTime = 2000;
+    private int tempInt = 123456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initData();
     }
-    private void initView()
-    {
+
+    private void initView() {
         danceNumberView = findViewById(R.id.activity_main_danceview);
-        danceNumberView.setText("123,456");
+        danceNumberView.setText(String.valueOf(tempInt));
         danceNumberView.start();
+    }
+
+    private void initData() {
         Handler h = new Handler();
-        Random random = new Random(1000);
+        Random random = new Random(47);
         h.postDelayed(new Runnable() {
             @Override
             public void run() {
-                danceNumberView.setText("4,323,222");
+                tempInt+=tempInt;
+                danceNumberView.setText(String.valueOf(tempInt));
                 danceNumberView.start();
+                h.postDelayed(this, delayTime);
             }
-        },2000);
+        }, delayTime);
     }
 }
